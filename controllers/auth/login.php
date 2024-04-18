057880ff2131d95abe6f58837a21636dfabc84ae
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-04-18 14:21:02 +0300

    sdads

--- controllers/auth/login.php
+++ controllers/auth/login.php
@@ -2,36 +2,38 @@
 
 guest();
 
-
 require "Validator.php";
 require "Database.php";
-$config = require("./config.php");
+$config = require("config.php");
 
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $db = new Database($config);
-    $errors = [];
-    dd($_POST);
-    if (!Validator::email($_POST["email"])) {
-        $errors["email"] = "Invalid email";
-    }
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $db = new Database($config);
 
-        $query = "SELECT * FROM users WHERE email = :email";
-        $params = [
-            ":email" => $_POST["email"]
-        ];
-        $user = $db->execute($query, $params)->fetch();
-        if (!$user || password_verify($_POST["password"], $user["password"])) {
-            $errors["email"] = "Kaut kas nav labi";
-        }
-        if (empty($errors)) {
-            $_SESSION["user"] = true;
-            $_SESSION["email"] = $_POST["email"];
-            header("Location: /");
-            die();
-    } 
+  $errors = [];
+
+  if (!Validator::email($_POST["email"])) {
+    $errors["email"] = "Nepareiz epasta formāts";
+  }
+
+  $query = "SELECT * FROM users WHERE email = :email";
+  $params = [
+    ":email" => $_POST["email"]
+  ];
+  $user = $db->execute($query, $params)->fetch();
+  if (!$user || !password_verify($_POST["password"], $user["password"])) {
+    $errors["email"] = "Kaut kas nav labi";
+  }
+
+  if (empty($errors)) {
+    $_SESSION["user"] = true;
+    $_SESSION["email"] = $_POST["email"];
+
+    header("Location: /");
+    die();
+  }
 }
 
-$title = "Login";    
+$title = "Login";
 require "views/posts/login.view.php";
 
-?>
+unset($_SESSION["flash"]);
--- controllers/auth/register.php
+++ controllers/auth/register.php
@@ -1,45 +1,40 @@
 <?php
 
-guest();
 
 require "Validator.php";
 require "Database.php";
-$config = require("./config.php");
+$config = require("config.php");
 
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $db = new Database($config);
-    $errors = [];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $db = new Database($config);
 
-    if (!Validator::email($_POST["email"])) {
-        $errors["email"] = "Invalid email";
-    }
+  $errors = [];
 
-    if (!Validator::password($_POST["password"])) {
-        $errors["password"] = "Invalid password";
-    }
-    
-    $query = "SELECT * FROM users WHERE email = email:";
-    $params = [":email" => $_POST["email"]];
-    $result = $db->execute($query, $params)->fetch();
+  if (!Validator::email($_POST["email"])) {
+    $errors["email"] = "Nepareiz epasta formāts";
+  }
+  if (!Validator::password($_POST["password"])) {
+    $errors["password"] = "Parolē ir nepilnības";
+  }
 
-    if ($result) {
-        $errors["email"] = "konts jau pastāv";
-    }
+  $query = "SELECT * FROM users WHERE email = :email";
+  $params = [":email" => $_POST["email"]];
+  $result = $db->execute($query, $params)->fetch();
 
-    if (empty($errors)) {
-        $query = "INSERT INTO users (email, password) VALUES (:email, :password)";
-        $params = [
-            ":email" => $_POST["email"],
-            ":password" => password_hash($_POST["password"], PASSWORD_BCRYPT)
-        ];
-        $db->execute($query, $params);
-    }
+  if ($result) {
+    $errors["email"] = "Konts jau pastāv";
+  }
 
-    $_SESSION["flash"] = "Tu esi veiksmīgi reģistrēts";
-    header("Location: /login");
-    die();
+  if (empty($errors)) {
+    $query = "INSERT INTO users (email, password) VALUES (:email, :password)";
+    $params = [
+      ":email" => $_POST["email"],
+      ":password" => password_hash($_POST["password"], PASSWORD_BCRYPT)
+    ];
+    $db->execute($query, $params);
+  }
 }
+
 
 $title = "Register";
 require "views/posts/register.view.php";
-?>
